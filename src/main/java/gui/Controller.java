54a324923581292a35692e3c4229c0eb0c04839b
Rikuklane <Rikuklane@gmail.com>
2021-03-18 12:03:03 +0200

    Added tree view to projects and tasks

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import logic.timeCalculator;
 
 import java.io.File;
@@ -22,11 +24,56 @@ public class Controller {
     @FXML
     private Label uiButton;
 
+    @FXML
+    private TreeView<String> projectsTree;
+
     // File to read from.
     private final File file = new File("records.csv");
 
     @FXML
     void initialize() {
+
+        // Test items for tree view
+        TreeItem<String> projects = new TreeItem<>("Projects");
+
+        TreeItem<String> project1 = new TreeItem<>("Project1");
+        TreeItem<String> project2 = new TreeItem<>("Project2");
+        TreeItem<String> project3 = new TreeItem<>("Project3");
+        TreeItem<String> project4 = new TreeItem<>("Project4");
+        TreeItem<String> archived = new TreeItem<>("Archived");
+
+        TreeItem<String> project1Task1 = new TreeItem<>("task1");
+        TreeItem<String> project1Task2 = new TreeItem<>("task2");
+        TreeItem<String> project2Task1 = new TreeItem<>("task3");
+        TreeItem<String> project2Task2 = new TreeItem<>("task4");
+        TreeItem<String> project3Task1 = new TreeItem<>("task5");
+        TreeItem<String> project3Task2 = new TreeItem<>("task6");
+        TreeItem<String> project4Task1 = new TreeItem<>("task7");
+        TreeItem<String> project4Task2 = new TreeItem<>("task8");
+
+        TreeItem<String> archivedProject1 = new TreeItem<>("ArchivedProject1");
+        TreeItem<String> archivedProject2 = new TreeItem<>("ArchivedProject2");
+
+        TreeItem<String> archivedTask1 = new TreeItem<>("ArchivedTask1");
+        TreeItem<String> archivedTask2 = new TreeItem<>("ArchivedTask2");
+        TreeItem<String> archivedTask3 = new TreeItem<>("ArchivedTask3");
+        TreeItem<String> archivedTask4 = new TreeItem<>("ArchivedTask4");
+
+        archivedProject1.getChildren().addAll(archivedTask1, archivedTask2);
+        archivedProject2.getChildren().addAll(archivedTask3, archivedTask4);
+
+        project1.getChildren().addAll(project1Task1, project1Task2);
+        project2.getChildren().addAll(project2Task1, project2Task2);
+        project3.getChildren().addAll(project3Task1, project3Task2);
+        project4.getChildren().addAll(project4Task1, project4Task2);
+        archived.getChildren().addAll(archivedProject1, archivedProject2);
+
+        projects.getChildren().addAll(project1, project2, project3, project4, archived);
+
+        projectsTree.setShowRoot(false);
+        projectsTree.setRoot(projects);
+        // Test items end for tree view
+
     }
 
     /**
@@ -60,5 +107,22 @@ public class Controller {
         series1.setName("Entry:" + numberOfRecords);
         series1.getData().add(new XYChart.Data("project", numberOfRecords));
         projectGraph.getData().add(series1);
+    }
+
+    /**
+     * Method for selecting projects and project tasks and sending out value.
+     */
+    public void selectItem() {
+        TreeItem<String> activity = projectsTree.getSelectionModel().getSelectedItem();
+        if (activity != null) {
+            System.out.println(activity.getValue());
+        }
+        // TODO list - Richard
+        // TODO return some value that can be used using selectItem() method
+        // TODO method createProjectItem
+        // TODO method createTaskItem
+        // TODO method archiveProject
+        // TODO method markTaskAsDone
+
     }
 }
