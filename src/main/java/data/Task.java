96765209c9c32c990367500fbaf3ba910ca3af5d
nbahh <smeshnik123@gmail.com>
2021-04-01 02:33:38 +0300

    Update Task.java
    -Fixed belongs field constructor assignment
    -Now holds records correctly (be changed in the future tho)

--- src/main/java/data/Task.java
+++ src/main/java/data/Task.java
@@ -7,17 +7,18 @@ import java.util.List;
 public class Task implements Data{
     private String taskName;
     private LocalDateTime creationDate;
-    private List<Record> records;
+    private List<String> records;
     private String belongs;
 
     public Task(String taskName, LocalDateTime creationDate, String belongs) {
         this.taskName = taskName;
         this.creationDate = creationDate;
         this.records = new ArrayList<>();
+        this.belongs = belongs;
     }
 
-    public void addRecord(Record record) {
-        this.records.add(record);
+    public void addRecord(String info) {
+        records.add(info);
     }
 
     public String getName() {
