d34404d04d9bccb16d3c6b6da6621e429e967df7
nbahh <smeshnik123@gmail.com>
2021-03-31 17:54:03 +0300

    Create Task class
    -Task class with information on the task name, creation date and currently holding records, also to which project it belongs.
    -Needs to be redesigned slightly later

---
+++ src/main/java/data/Task.java
@@ -0,0 +1,31 @@
+package data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import com.github.cliftonlabs.json_simple.JsonObject;
+
+public class Task implements Data{
+    private String taskName;
+    private LocalDateTime creationDate;
+    private List<Record> records;
+    private String belongs;
+
+    public Task(String taskName, LocalDateTime creationDate, String belongs) {
+        this.taskName = taskName;
+        this.creationDate = creationDate;
+        this.records = new ArrayList<>();
+    }
+
+    public void addRecord(Record record) {
+        this.records.add(record);
+    }
+
+    public String getName() {
+        return this.taskName;
+    }
+
+    public String getBelongs() {
+        return belongs;
+    }
+}
