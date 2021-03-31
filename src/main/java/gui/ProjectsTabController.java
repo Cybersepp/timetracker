6b75ffc756fc20142b62d0d5ca73d71ba7c17ce3
nbahh <smeshnik123@gmail.com>
2021-04-01 02:35:32 +0300

    Update ProjectsTabController.java
    -Pre-created project and tasks are added to their corresponding storage places
    (Project get added to DataHandler, task to their corresponding projects)

--- src/main/java/gui/ProjectsTabController.java
+++ src/main/java/gui/ProjectsTabController.java
@@ -7,6 +7,10 @@ import gui.treeItems.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import data.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDateTime;
 
 public class ProjectsTabController {
 
@@ -29,28 +33,53 @@ public class ProjectsTabController {
         // Project items
         ProjectTreeItem project1 = new ProjectTreeItem("Project One");
         projects.getChildren().add(project1);
+        Project newProject1 = new Project("Project One", LocalDateTime.now());
+
         ProjectTreeItem project2 = new ProjectTreeItem("Project Two");
         projects.getChildren().add(project2);
+        Project newProject2 = new Project("Project Two", LocalDateTime.now());
+
         ProjectTreeItem project3 = new ProjectTreeItem("Project Three");
         projects.getChildren().add(project3);
+        Project newProject3 = new Project("Project Three", LocalDateTime.now());
 
-        TaskTreeItem task1 = new TaskTreeItem("task1");
+        DataHandler.addProject(newProject1);
+        DataHandler.addProject(newProject2);
+        DataHandler.addProject(newProject3);
+
+        TaskTreeItem task1 = new TaskTreeItem("Demo Task 1");
         project1.getChildren().add(task1);
+        Task newTask11 = new Task("Demo Task 1", LocalDateTime.now(), "Project One");
 
-        TaskTreeItem task2 = new TaskTreeItem("task2");
+        TaskTreeItem task2 = new TaskTreeItem("Demo Task 2");
         project1.getChildren().add(task2);
+        Task newTask12 = new Task("Demo Task 2", LocalDateTime.now(), "Project One");
 
-        TaskTreeItem task3 = new TaskTreeItem("task3");
+        TaskTreeItem task3 = new TaskTreeItem("Demo Task 1");
         project2.getChildren().add(task3);
+        Task newTask21 = new Task("Demo Task 1", LocalDateTime.now(), "Project Two");
 
-        TaskTreeItem task4 = new TaskTreeItem("task4");
+        TaskTreeItem task4 = new TaskTreeItem("Demo Task 2");
         project2.getChildren().add(task4);
+        Task newTask22 = new Task("Demo Task 2", LocalDateTime.now(), "Project Two");
 
-        TaskTreeItem task5 = new TaskTreeItem("task5");
+        TaskTreeItem task5 = new TaskTreeItem("Demo Task 1");
         project3.getChildren().add(task5);
+        Task newTask31 = new Task("Demo Task 1", LocalDateTime.now(), "Project Three");
 
-        TaskTreeItem task6 = new TaskTreeItem("task6");
+        TaskTreeItem task6 = new TaskTreeItem("Demo Task 2");
         project3.getChildren().add(task6);
+        Task newTask32 = new Task("Demo Task 2", LocalDateTime.now(), "Project Three");
+
+        newProject1.addTask(newTask11);
+        newProject1.addTask(newTask12);
+
+        newProject2.addTask(newTask21);
+        newProject2.addTask(newTask22);
+
+        newProject3.addTask(newTask31);
+        newProject3.addTask(newTask32);
+
 
         // Archived items
 
@@ -68,6 +97,7 @@ public class ProjectsTabController {
         archivedProject2.getChildren().add(aTask4);
 
 
+
         // tree configuration
         projectsTree.setShowRoot(false);
         projectsTree.setRoot(root);
@@ -83,8 +113,12 @@ public class ProjectsTabController {
         TreeItem<String> activity = projectsTree.getSelectionModel().getSelectedItem();
         if (activity != null) {
 //            System.out.println(activity.getValue());
-            DataHandler.currentlyChosenTask = DataHandler.getTaskByName(activity.getValue());
-            DataHandler.showCurrentlyChosen();
+            try {
+                DataHandler.currentlyChosenTask = DataHandler.getTaskByName(activity.getValue());
+                DataHandler.showCurrentlyChosen();
+            } catch (Exception e) {
+                System.out.println("This is not a task");
+            }
         }
         // TODO return some value that can be used using selectItem() method
     }
