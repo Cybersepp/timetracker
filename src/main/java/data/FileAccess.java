279ab1c153bf4d2124cfcb4e1ab46f59384c0f37
nbahh <smeshnik123@gmail.com>
2021-04-25 03:59:41 +0300

    Update FileAccess.java
    File saving and reading format change

--- src/main/java/data/FileAccess.java
+++ src/main/java/data/FileAccess.java
@@ -71,7 +71,7 @@ public class FileAccess {
         return taskMap;
     }
 
-    public static Map<String, Map<>> getProjectData() {
+    public static Map<String, Map<String, Object>> getProjectData() {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
 
