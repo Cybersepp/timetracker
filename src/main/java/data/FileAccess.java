f24b031d39ab9151814d9110b987036fa3944fb3
nbahh <smeshnik123@gmail.com>
2021-05-12 03:41:48 +0300

    Update FileAccess.java
    SaveData method rewritten to new format

--- src/main/java/data/FileAccess.java
+++ src/main/java/data/FileAccess.java
@@ -1,9 +1,11 @@
 package data;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import gui.controllers.ProjectsTabController;
 import gui.popups.notification.ErrorPopup;
 import logic.treeItems.ProjectTreeItem;
@@ -19,29 +21,44 @@ public class FileAccess {
         // util methods
     }
 
+    public static void saveData() {
+        // I think here LinkedHashMap is absolutely useless, but I left it like this in case you still need specifically
+        // linked map
+        Map<String, List<ProjectTreeItem>> mainMap = Map.of("projects", ProjectsTabController.getActiveRoot().getJuniors());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        ObjectWriter writer = objectMapper.writer(new DefaultPrettyPrinter());
+        try {
+            writer.writeValue(Paths.get("data.json").toFile(), mainMap);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     //TODO Would be nice if you could somehow append a single thing somewhere in the data.json for example a task to an existing project
     // - Is it possible?
     // - Is it necessary?
     // - How much time would it save?
     // - Thinking about it because if there are loads of recordings, would the constant overwriting of data make it too slow?
-    public static void saveData() {
-        try {
-            Map<String, Object> dataMap = new LinkedHashMap<>();
-
-            List<ProjectTreeItem> currentProjects = new ArrayList<>(ProjectsTabController.getActiveRoot().getJuniors());
-            currentProjects.addAll(ProjectsTabController.getArchivedRoot().getJuniors());
-
-            for (ProjectTreeItem project : currentProjects) {
-                dataMap.put(project.getValue(), getProjectMap(project));
-            }
-
-            ObjectMapper mapper = new ObjectMapper();
-            ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
-            writer.writeValue(Paths.get("data.json").toFile(), dataMap);
-        } catch (Exception e) {
-            new ErrorPopup("Could not write to file: " + e);
-        }
-    }
+//    public static void saveData() {
+//        try {
+//            Map<String, Object> dataMap = new LinkedHashMap<>();
+//
+//            List<ProjectTreeItem> currentProjects = new ArrayList<>(ProjectsTabController.getActiveRoot().getJuniors());
+//            currentProjects.addAll(ProjectsTabController.getArchivedRoot().getJuniors());
+//
+//            for (ProjectTreeItem project : currentProjects) {
+//                dataMap.put(project.getValue(), getProjectMap(project));
+//            }
+//
+//            ObjectMapper mapper = new ObjectMapper();
+//            ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+//            writer.writeValue(Paths.get("data.json").toFile(), dataMap);
+//        } catch (Exception e) {
+//            new ErrorPopup("Could not write to file: " + e);
+//        }
+//    }
 
     public static Map<String, Object> getProjectMap(ProjectTreeItem project) {
         Map<String, Object> taskMap = new LinkedHashMap<>();
