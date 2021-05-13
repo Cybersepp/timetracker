80d4baffd3a853215e47a243103de383ada5a1a8
nbahh <smeshnik123@gmail.com>
2021-05-13 05:04:39 +0300

    Working deserialization from JSON
    but still includes some sout from debugging

--- src/main/java/data/FileAccess.java
+++ src/main/java/data/FileAccess.java
@@ -11,6 +11,7 @@ import gui.popups.notification.ErrorPopup;
 import logic.treeItems.ProjectTreeItem;
 import logic.treeItems.TaskTreeItem;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.*;
@@ -36,80 +37,25 @@ public class FileAccess {
         }
     }
 
-    //TODO Would be nice if you could somehow append a single thing somewhere in the data.json for example a task to an existing project
-    // - Is it possible?
-    // - Is it necessary?
-    // - How much time would it save?
-    // - Thinking about it because if there are loads of recordings, would the constant overwriting of data make it too slow?
-//    public static void saveData() {
-//        try {
-//            Map<String, Object> dataMap = new LinkedHashMap<>();
-//
-//            List<ProjectTreeItem> currentProjects = new ArrayList<>(ProjectsTabController.getActiveRoot().getJuniors());
-//            currentProjects.addAll(ProjectsTabController.getArchivedRoot().getJuniors());
-//
-//            for (ProjectTreeItem project : currentProjects) {
-//                dataMap.put(project.getValue(), getProjectMap(project));
-//            }
-//
-//            ObjectMapper mapper = new ObjectMapper();
-//            ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
-//            writer.writeValue(Paths.get("data.json").toFile(), dataMap);
-//        } catch (Exception e) {
-//            new ErrorPopup("Could not write to file: " + e);
-//        }
-//    }
+    public static Map<String, List<ProjectTreeItem>> getData() {
 
-    public static Map<String, Object> getProjectMap(ProjectTreeItem project) {
-        Map<String, Object> taskMap = new LinkedHashMap<>();
+        System.out.println("help");
+        ObjectMapper objectMapper = new ObjectMapper();
+        File file = new File("data.json");
 
-        taskMap.put("Tasks", getTaskMap(project));
-        taskMap.put("isArchived", project.isArchived());
-
-        return taskMap;
-    }
-
-    public static Map<String, Map<String, Object>> getTaskMap(ProjectTreeItem project) {
-        Map<String, Map<String, Object>> taskMap = new LinkedHashMap<>();
-        List<TaskTreeItem> projectTaskList = project.getJuniors();
-
-        projectTaskList.forEach((t -> taskMap.put(t.getValue(), getTaskAttributesMap(t))));
-
-        return taskMap;
-    }
-
-    public static Map<String, Object> getTaskAttributesMap(TaskTreeItem task) {
-        Map<String, Object> taskAttributesMap = new LinkedHashMap<>();
-
-        taskAttributesMap.put("isDone", task.isDone());
-
-        var recordings = new ArrayList<String>();
-        for (Recording recording : task.getRecordings()) {
-            recordings.add(recording.getRecordInfo());
-        }
-        taskAttributesMap.put("Records", recordings);
-
-        return taskAttributesMap;
-    }
-
-    //FIXME idk if this is the best way for this - made by Richard
-    public static Map<String, String> getRecordAttributesMap(TaskTreeItem task) {
-        Map<String, String> recordMap = new HashMap<>();
-
-        var recordings = task.getRecordings();
-        recordings.forEach((recording -> recordMap.put("Record", recording.getRecordInfo())));
-        //FIXME this might be broken
-        return recordMap;
-    }
-
-    public static Map<String, Map<String, Object>> getProjectData() {
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            //FIXME check if the file exists and then return null here instead of Exception
-            return objectMapper.readValue(Paths.get("data.json").toFile(), new TypeReference<>(){});
-        } catch (IOException e) {
+//            Map<String, List<ProjectTreeItem>> mainMap = objectMapper.readValue(file, new TypeReference<Map<String, List<ProjectTreeItem>>>() {});
+            Map<String, List<ProjectTreeItem>> dataMap = objectMapper.readValue(Paths.get("data.json").toFile(),
+                    new TypeReference<Map<String, List<ProjectTreeItem>>>(){});
+
+            List<ProjectTreeItem> projects = dataMap.get("projects");
+
+            projects.forEach((project) -> System.out.println(project.getJuniors()));
             return null;
-            // TODO Should it also have a WarningPopup occur if some exception other than no file found occurs?
+        } catch (IOException e) {
+            System.out.println("Error during file reading: " + e.getMessage());
         }
+        return null;
+
     }
 }
