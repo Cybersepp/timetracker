f0cfac86be7a467ca8e986b00bad0b781c2e22c1
cybersepp <georg.sumailov@ut.ee>
2021-05-18 11:54:10 +0300

    Graph tooltip more informative

--- src/main/java/logic/BarChartToolTip.java
+++ src/main/java/logic/BarChartToolTip.java
@@ -11,17 +11,71 @@ public class BarChartToolTip extends Tooltip {
     }
 
     private void tooltipSetup(String seconds, String projectName) {
-        String minutes = String.valueOf(Math.round(Float.parseFloat(seconds)) / 60);
-        String hours = String.valueOf(Math.round(Float.parseFloat(minutes)) / 60);
-        String text = (projectName + ": "  + "\n" +
-                seconds + " seconds" + "\n" +
-                minutes + " minutes" + "\n" +
-                hours + " hours");
         styleProperty().set("-fx-font: normal bold 12 Langdon; "
                 + "-fx-base: black; "
                 + "-fx-text-fill: orange;"
-                +  "-fx-stroke-width: 5");
-        setText(text);
+                + "-fx-stroke-width: 5");
+        setText(projectName + ": " + "\n" + calculateTime(seconds));
         setShowDelay(Duration.ZERO);
+    }
+
+    /**
+     * Calculates time to show for the graph tooltip.
+     * @param duration in seconds.
+     * @return a string which shows time.
+     */
+    private String calculateTime(String duration) {
+        StringBuilder builder = new StringBuilder();
+        int seconds = Integer.parseInt(duration);
+        int minutes = seconds / 60;
+        int hours = minutes / 60;
+
+        if (minutes != 0) {
+            seconds -= minutes * 60;
+            if (hours != 0) {
+                minutes -= hours * 60;
+                if (hours > 24) {
+                    int days = hours / 24;
+                    if (isSingular(days)) {
+                        builder.append(days).append(" day ");
+                    } else {
+                        builder.append(days).append(" days ");
+                    }
+                    hours -= days * 24;
+                }
+                if (isSingular(hours)) {
+                    builder.append(hours).append(" hour ");
+                } else {
+                    builder.append(hours).append(" hours ");
+                }
+                if (isSingular(minutes)) {
+                    builder.append(minutes).append(" minute ");
+                } else {
+                    builder.append(minutes).append(" minutes ");
+                }
+            } else {
+                if (isSingular(minutes)) {
+                    builder.append(minutes).append(" minute ");
+                } else {
+                    builder.append(minutes).append(" minutes ");
+                }
+            }
+        }
+
+        if (isSingular(seconds)) {
+            builder.append(seconds).append(" second ");
+        } else {
+            builder.append(seconds).append(" seconds ");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Method to check if there is only one time unit or many.
+     * @param time unit.
+     * @return true if it is in singular.
+     */
+    private boolean isSingular(int time) {
+        return time == 1;
     }
 }
